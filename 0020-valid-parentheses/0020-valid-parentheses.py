a8d76624c94a9aad8db3689d164dbbebc1433fc1
Julian Castro <jcas2tur@gmail.com>
2023-10-08 13:30:27 -0400

    Time: 41 ms (42.14%), Space: 16.3 MB (36.57%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.py
+++ 0020-valid-parentheses/0020-valid-parentheses.py
@@ -4,9 +4,9 @@ class Solution:
         for p in s:
             if p == '(' or p == '{' or p == '[':
                 stack.append(p)
-            elif len(stack) > 0 and ((p == ')' and stack[-1] == '(') or (p == '}' and stack[-1] == '{') or (p == ']' and stack[-1] == '[')):
+            elif stack and ((p == ')' and stack[-1] == '(') or (p == '}' and stack[-1] == '{') or (p == ']' and stack[-1] == '[')):
                 stack.pop()
             else:
                 return False
-        return False if len(stack) > 0 else True
+        return False if stack else True
         
