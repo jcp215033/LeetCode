73e63e81b023cae00eef2e9cdbac604782f9df6f
Julian Castro <jcas2tur@gmail.com>
2023-10-12 11:52:03 -0400

    Time: 106 ms (11.12%), Space: 47.8 MB (24.73%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.js
+++ 0002-add-two-numbers/0002-add-two-numbers.js
@@ -15,18 +15,22 @@ var addTwoNumbers = function(l1, l2) {
     let head = ans
     let rem = 0;
     while (l1 || l2) {
-        let node;
-        if (l1 && l2) {
-            node = l1.val + l2.val + rem;
-            l1 = l1.next
-            l2 = l2.next
-        } else if (l1) {
-            node = l1.val + rem;
-            l1 = l1.next
-        } else {
-            node = l2.val + rem;
-            l2 = l2.next
-        }
+        let node = (l1 ? l1.val : 0) + (l2 ? l2.val : 0) + rem
+        l1 = l1 && l1.next ? l1.next : null
+        l2 = l2 && l2.next ? l2.next : null
+        // node += l2 ? l2.val : 0
+        // node += rem
+        // if (l1 && l2) {
+        //     node = l1.val + l2.val + rem;
+        //     l1 = l1.next
+        //     l2 = l2.next
+        // } else if (l1) {
+        //     node = l1.val + rem;
+        //     l1 = l1.next
+        // } else {
+        //     node = l2.val + rem;
+        //     l2 = l2.next
+        // }
         ans.next = new ListNode(node % 10)
         rem = (node / 10) >> 0
         ans = ans.next
