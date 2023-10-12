d7e9956f325609b1b5a0b381ad526b7f08a926b6
Julian Castro <jcas2tur@gmail.com>
2023-10-12 09:08:37 -0400

    Time: 53 ms (64.63%), Space: 43.5 MB (24.68%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.js
@@ -11,34 +11,17 @@
  * @return {ListNode}
  */
 var removeNthFromEnd = function(head, n) {
-    let l = head, r = head;
-    for (let i = 0; i < n; i++) {
+    let dummy = new ListNode(0);
+    dummy.next = head;
+    let l = dummy, r = dummy;
+    for (let i = 0; i <= n; i++) {
         r = r.next
     }
-
-    while (r && r.next) {
+    while(r) {
+        r = r.next
         l = l.next
-        r = r.next
     }
-    console.log(l)
-    console.log(r)
     
-    if (l.next && l.next.next && r) {
-        console.log(1)
-        l.next = l.next.next
-        return head
-    }
-    else if (!l.next) {
-        console.log(2)
-        return null
-    } else if (!l.next.next && n === 1) {
-        console.log(3)
-        l.next = null
-        return head
-    } else if (!l.next.next || l === head) {
-        console.log(4)
-        head = head.next
-        return head
-    } 
-    return head
+    l.next = l.next.next
+    return dummy.next
 };
