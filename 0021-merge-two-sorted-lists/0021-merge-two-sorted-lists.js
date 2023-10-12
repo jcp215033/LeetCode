4e3d087c8361fa7276015d392ca412a79a5d902f
Julian Castro <jcas2tur@gmail.com>
2023-10-11 22:39:11 -0400

    Time: 57 ms (65.47%), Space: 44 MB (66.08%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -11,36 +11,21 @@
  * @return {ListNode}
  */
 var mergeTwoLists = function(list1, list2) {
-    if (!list1) return list2;
-    if (!list2) return list1;
+    let dummy = new ListNode();
+    let tail = dummy;
 
-    let head, curr, n1, n2;
-    if (list1.val <= list2.val) {
-        curr = list1
-        head = list1
-        n1 = curr.next
-        n2 = list2
-    } else {
-        curr = list2
-        head = list2
-        n2 = curr.next
-        n1 = list1
-    }
-
-    while (n1 !== null && n2 !== null) {
-        if (n1.val <= n2.val) {
-            curr.next = n1
-            curr = n1
-            n1 = n1.next
-        } else if (n1.val > n2.val) {
-            curr.next = n2
-            curr = n2
-            n2 = n2.next
+    while (list1 !== null && list2 !== null) {
+        if (list1.val < list2.val) {
+            tail.next = list1
+            list1 = list1.next
+        } else {
+            tail.next = list2
+            list2 = list2.next
         }
+        tail = tail.next
     }
 
-    if (n1 !== null) curr.next = n1
-    else curr.next = n2
-    return head
-    // console.log(head)
+    if (list1 !== null) tail.next = list1
+    else tail.next = list2
+    return dummy.next
 };
