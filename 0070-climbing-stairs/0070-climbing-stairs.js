0f34d0a4d5f4ac79fb28fc112a322e3c6331816f
Julian Castro <jcas2tur@gmail.com>
2023-10-13 14:49:28 -0400

    Time: 53 ms (38.75%), Space: 41.8 MB (36.73%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    let memo = {}
+    function helper(n) {
+        if (n === 1) return 1
+        else if (n === 2) return 2
+        else if (memo[n]) {
+            return memo[n]
+        }
+        memo[n - 1] = helper(n - 1);
+        memo[n - 2] = helper(n - 2)
+        return memo[n - 1] + memo[n - 2]
+    }
+    return helper(n)
+};
