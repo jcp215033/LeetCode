33ead522d74aece00fc6f69468fc2da1bd5c50c9
Julian Castro <jcas2tur@gmail.com>
2023-10-14 09:32:55 -0400

    Time: 66 ms (61.85%), Space: 17.6 MB (42.95%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hash = {}
+        for i in range(len(nums)):
+            if target - nums[i] in hash:
+                return [i, hash[target - nums[i]]]
+            else:
+                hash[nums[i]] = i
