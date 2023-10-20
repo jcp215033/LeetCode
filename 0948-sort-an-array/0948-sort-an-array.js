ac912ba10d569b0384db7e6f2015e5f94507b0b3
Julian Castro <jcas2tur@gmail.com>
2023-10-20 11:05:17 -0400

    Time: 216 ms (72.33%), Space: 71.1 MB (42.89%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.js
+++ 0948-sort-an-array/0948-sort-an-array.js
@@ -3,14 +3,42 @@
  * @return {number[]}
  */
 var sortArray = function(nums) {
-    for (let i = 1; i < nums.length; i++) {
-        let j = i - 1
-        while (j >= 0 && nums[j + 1] < nums[j]) {
-            let temp = nums[j + 1]
-            nums[j + 1] = nums[j]
-            nums[j] = temp
-            j--
+    function merge(arr, l, m, r) {
+        let left = arr.slice(l, m + 1)
+        let right = arr.slice(m + 1, r + 1)
+        let i = l, j = 0, k = 0
+        while (j < left.length && k < right.length) {
+            if (left[j] <= right[k]) {
+                arr[i] = left[j]
+                j++
+            } else {
+                arr[i] = right[k]
+                k++
+            }
+            i++
+        }
+        while (j < left.length) {
+            arr[i] = left[j]
+            j++
+            i++
+        }
+        while(k < right.length) {
+            arr[i] = right[k]
+            k++
+            i++
         }
     }
-    return nums
+    function mergeSort(arr, l, r) {
+        if (l === r) {
+            return arr
+        }
+
+        let m = Math.floor((l + r) / 2)
+        mergeSort(arr, l, m)
+        mergeSort(arr, m + 1, r)
+        merge(arr, l, m, r)
+        return arr
+    }
+
+    return mergeSort(nums, 0, nums.length + 1)
 };
