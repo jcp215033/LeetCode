b4acd15872813dd8d200fb9b9888f170e944bb8b
Julian Castro <jcas2tur@gmail.com>
2023-10-20 11:52:26 -0400

    Time: 82 ms (67.9%), Space: 47.2 MB (82.08%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.js
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.js
@@ -9,30 +9,43 @@
  * @param {ListNode[]} lists
  * @return {ListNode}
  */
-var mergeTwoLists = function(list1, list2) {
-    let dummy = new ListNode();
-    let tail = dummy;
-
-    while (list1 !== null && list2 !== null) {
-        if (list1.val < list2.val) {
-            tail.next = list1
-            list1 = list1.next
-        } else {
-            tail.next = list2
-            list2 = list2.next
-        }
-        tail = tail.next
-    }
-
-    if (list1 !== null) tail.next = list1
-    else tail.next = list2
-    return dummy.next
-};
-
 var mergeKLists = function(lists) {
     
-    while(lists.length > 1) {
-        lists.push(mergeTwoLists(lists.pop(), lists.pop()))
+    function mergeLists(list1, list2){
+        let dummy = new ListNode()
+        let tail = dummy
+        while (list1 && list2) {
+            if (list1.val < list2.val) {
+                tail.next = list1
+                list1 = list1.next
+            } else {
+                tail.next = list2
+                list2 = list2.next
+            }
+            tail = tail.next
+        }
+        if (list1) {
+            tail.next = list1
+        } else if (list2) {
+            tail.next = list2
+        }
+
+        return dummy.next
     }
-    return lists[0] || null
+    
+    function mergeSort(arr, l, r) {
+        if (l === r) {
+            return arr[l]
+        }
+        let m = Math.floor((l + r) / 2)
+        let l1 = mergeSort(arr, l, m)
+        let l2 = mergeSort(arr, m + 1, r)
+        return mergeLists(l1, l2)
+
+    }
+
+    if (lists.length === 0) return null
+
+    return mergeSort(lists, 0, lists.length - 1)
+
 };
