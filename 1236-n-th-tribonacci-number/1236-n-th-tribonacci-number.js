6ded241b04bec7088b27f14083bea4e246ad4109
Julian Castro <jcas2tur@gmail.com>
2023-10-21 09:39:04 -0400

    Time: 45 ms (82.44%), Space: 41.4 MB (84.05%) - LeetHub

--- 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.js
+++ 1236-n-th-tribonacci-number/1236-n-th-tribonacci-number.js
@@ -1,19 +1,14 @@
-/**
- * @param {number} n
- * @return {number}
- */
 var tribonacci = function(n) {
-    let memo = {}
-    function helper (n) {
-        if (n === 0) {
-            return 0
-        } else if (n < 3) {
-            return 1
-        } else if (memo[n]) {
-            return memo[n]
-        }
-        memo[n] = helper(n - 3) + helper(n - 2) + helper(n - 1)
-        return memo[n]
+    let menu = {
+        0: 0,
+        1: 1,
+        2: 1
     }
-    return helper(n)
+    let dp = (n) => {
+        if (menu[n] !== undefined) {
+            return menu[n];
+        }
+        return menu[n] = dp(n-1)+dp(n-2)+dp(n-3);
+    }
+    return dp(n);
 };
