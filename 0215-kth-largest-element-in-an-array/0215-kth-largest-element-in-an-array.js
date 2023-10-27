89fbbeb1006f21d13e7371d3f7a0b7c765ecace8
Julian Castro <jcas2tur@gmail.com>
2023-10-27 15:58:35 -0400

    Time: 158 ms (42.15%), Space: 52.4 MB (66.74%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
@@ -4,40 +4,5 @@
  * @return {number}
  */
 var findKthLargest = function(nums, k) {
-    function merge(arr, l, m, r) {
-        let left = arr.slice(l, m + 1)
-        let right = arr.slice(m + 1, r + 1)
-        let i = l, j = 0, k = 0
-        while (j < left.length && k < right.length) {
-            if (left[j] >= right[k]) {
-                arr[i] = left[j]
-                j++
-            } else {
-                arr[i] = right[k]
-                k++
-            }
-            i++
-        }
-        while (j < left.length) {
-            arr[i] = left[j]
-            j++
-            i++
-        }
-        while(k < right.length) {
-            arr[i] = right[k]
-            k++
-            i++
-        }
-    }
-    function mergeSort(arr, l, r) {
-        if (l === r) return arr
-
-        let m = Math.floor((l + r) / 2)
-        mergeSort(arr, l, m)
-        mergeSort(arr, m + 1, r)
-        merge(arr, l, m, r)
-        return arr
-    }
-
-    return mergeSort(nums, 0, nums.length + 1)[k - 1]
+    return nums.sort((a,b) => b - a)[k - 1]
 };
