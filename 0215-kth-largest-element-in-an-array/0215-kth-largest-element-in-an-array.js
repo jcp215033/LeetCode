b751d9cb3bd93dedeac0e80038543b377c6475c6
Julian Castro <jcas2tur@gmail.com>
2023-10-27 19:48:43 -0400

    Time: 150 ms (51.84%), Space: 52.5 MB (66.74%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
@@ -4,5 +4,28 @@
  * @return {number}
  */
 var findKthLargest = function(nums, k) {
-    return nums.sort((a,b) => b - a)[k - 1]
+    return nums
+    .sort((a, b) => a - b)
+    .reverse()
+    .slice(k - 1)
+    .shift()
+    // k = nums.length - k
+    // function quickSelect(l, r) {
+    //     let p = l
+    //     for (let i = l; i < r; i++) {
+    //         if(nums[i] <= nums[r]) {
+    //             [nums[i], nums[p]] = [nums[p], nums[i]]
+    //             p++
+    //         }
+    //     }
+    //     [nums[r], nums[p]] = [nums[p], nums[r]]
+    //     if (p > k) {
+    //         return quickSelect(l, p - 1)
+    //     } else if (p < k) {
+    //         return quickSelect(p + 1, r)
+    //     } else {
+    //         return nums[p]
+    //     }
+    // }
+    // return quickSelect(0, nums.length - 1)
 };
