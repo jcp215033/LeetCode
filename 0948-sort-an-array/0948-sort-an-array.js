d6fabbe7187704cb7d2d298f979130ce7c381aa7
Julian Castro <jcas2tur@gmail.com>
2023-10-27 11:21:45 -0400

    Time: 3134 ms (28.96%), Space: 69.4 MB (55%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.js
+++ 0948-sort-an-array/0948-sort-an-array.js
@@ -1,32 +1,40 @@
-/**
- * @param {number[]} nums
- * @return {number[]}
- */
 var sortArray = function(nums) {
-    function quickSort(arr, l, r) {
-        if (r - l + 1 <= 1) {
-            return arr
-        }
+    function insertionSort(arr, l, r) {
+        for (let i = l + 1; i <= r; i++) {
+            let key = arr[i];
+            let j = i - 1;
 
-        let pivot = arr[r], left = l;
-
-        for (let i = l; i < r; i++) {
-            if (arr[i] < pivot) {
-                let temp = arr[i]
-                arr[i] = arr[left]
-                arr[left] = temp
-                left++
+            while (j >= l && arr[j] > key) {
+                arr[j + 1] = arr[j];
+                j--;
             }
+            arr[j + 1] = key;
         }
-
-        arr[r] = arr[left]
-        arr[left] = pivot
-
-        quickSort(arr, l, left - 1)
-        quickSort(arr, left + 1, r)
-
-        return arr
     }
 
-    return quickSort(nums, 0, nums.length - 1)
+    function quickSort(arr, l, r) {
+        if (r - l + 1 <= 10) {
+            insertionSort(arr, l, r);
+            return;
+        }
+
+        let pivotIndex = l + Math.floor(Math.random() * (r - l + 1));
+        let pivot = arr[pivotIndex];
+        [arr[pivotIndex], arr[r]] = [arr[r], arr[pivotIndex]]; // swap pivot with last element
+
+        let left = l;
+        for (let i = l; i < r; i++) {
+            if (arr[i] < pivot) {
+                [arr[i], arr[left]] = [arr[left], arr[i]]; // swap
+                left++;
+            }
+        }
+        [arr[r], arr[left]] = [arr[left], arr[r]]; // swap pivot to its correct position
+
+        quickSort(arr, l, left - 1);
+        quickSort(arr, left + 1, r);
+    }
+
+    quickSort(nums, 0, nums.length - 1);
+    return nums;
 };
