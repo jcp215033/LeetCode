538b1b9cfd2699604f4f73c85ad0f8dbda29503b
Julian Castro <jcas2tur@gmail.com>
2023-10-27 19:50:08 -0400

    Time: 243 ms (27.81%), Space: 71.3 MB (8%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.js
@@ -4,28 +4,14 @@
  * @return {number}
  */
 var findKthLargest = function(nums, k) {
-    return nums
-    .sort((a, b) => a - b)
-    .reverse()
-    .slice(k - 1)
-    .shift()
-    // k = nums.length - k
-    // function quickSelect(l, r) {
-    //     let p = l
-    //     for (let i = l; i < r; i++) {
-    //         if(nums[i] <= nums[r]) {
-    //             [nums[i], nums[p]] = [nums[p], nums[i]]
-    //             p++
-    //         }
-    //     }
-    //     [nums[r], nums[p]] = [nums[p], nums[r]]
-    //     if (p > k) {
-    //         return quickSelect(l, p - 1)
-    //     } else if (p < k) {
-    //         return quickSelect(p + 1, r)
-    //     } else {
-    //         return nums[p]
-    //     }
-    // }
-    // return quickSelect(0, nums.length - 1)
+    const minHeap = new MinPriorityQueue()
+
+    for (const num of nums) {
+        minHeap.enqueue(num);
+
+        const isAtCapacity = k < minHeap.size();
+        if (isAtCapacity) minHeap.dequeue();
+    }
+
+    return minHeap.front().element
 };
