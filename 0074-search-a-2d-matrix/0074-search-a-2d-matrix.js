f7f76ae18778f0d4e5bc90a3553756c3345db054
Julian Castro <jcas2tur@gmail.com>
2023-10-28 10:13:21 -0400

    Time: 53 ms (49.22%), Space: 42 MB (51.59%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.js
@@ -4,13 +4,32 @@
  * @return {boolean}
  */
 var searchMatrix = function(matrix, target) {
-    matrix = matrix.flat()
+    // if (!matrix || matrix.length === 0 || matrix[0].length === 0) return false;
+
     let l = 0, r = matrix.length - 1, m;
+
     while (l <= r) {
-        m = Math.floor((l + r) / 2)
-        if (matrix[m] < target) l = m + 1
-        else if (matrix[m] > target) r = m - 1
-        else return true
+        m = Math.floor((l + r) / 2);
+        if (matrix[m][0] === target) return true;
+
+        if (matrix[m][0] < target) {
+            if (!matrix[m + 1] || matrix[m + 1][0] > target) break;
+            else l = m + 1;
+        }
+        else {
+            r = m - 1;
+        }
     }
-    return false
+
+    // Now, search the row 'm' for the target
+    let l2 = 0, r2 = matrix[m].length - 1, m2;
+    while (l2 <= r2) {
+        m2 = Math.floor((l2 + r2) / 2);
+        if (matrix[m][m2] === target) return true;
+
+        if (matrix[m][m2] < target) l2 = m2 + 1;
+        else r2 = m2 - 1;
+    }
+
+    return false;
 };
