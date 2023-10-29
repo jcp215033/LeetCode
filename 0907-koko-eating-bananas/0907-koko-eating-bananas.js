1654ac93a094cfc6ee92a567e06a8ff65d48a758
Julian Castro <jcas2tur@gmail.com>
2023-10-29 11:16:10 -0400

    Time: 55 ms (96.23%), Space: 45.1 MB (67.96%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.js
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.js
@@ -6,15 +6,11 @@
 var minEatingSpeed = function(piles, h) {
     let L = 1, R = Math.max(...piles);
     while (L <= R) {
-        let K = Math.floor((L + R) / 2)
+        let K = Math.floor((L + R) / 2);
         let hours = 0;
-        for (let i = 0; i < piles.length; i++) {
-            hours += Math.ceil(piles[i] / K);
-            // if (piles[i] <= K) hours += 1
-            // else hours += Math.ceil(piles[i] / K)
-        }
-        if (hours > h) L = K + 1
-        else R = K - 1
-    }
-    return L
+        for (let i = 0; i < piles.length; i++) hours += Math.ceil(piles[i] / K);
+        if (hours > h) L = K + 1;
+        else R = K - 1;
+    };
+    return L;
 };
