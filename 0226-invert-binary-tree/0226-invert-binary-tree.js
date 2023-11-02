3d3e730d1a64e1946590bea79f9ff986e73a2eed
Julian Castro <jcas2tur@gmail.com>
2023-11-02 09:42:58 -0400

    Time: 48 ms (77.7%), Space: 42.1 MB (71.25%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.js
+++ 0226-invert-binary-tree/0226-invert-binary-tree.js
@@ -11,11 +11,11 @@
  * @return {TreeNode}
  */
 var invertTree = function(root) {
-    if (!root) return null
-    let tmp = root.right
-    root.right = root.left
-    root.left = tmp
-    invertTree(root.left)
-    invertTree(root.right)
-    return root
+    if (root === null) {
+    return null
+  }
+  let temp = root.left
+  root.left = invertTree(root.right)
+  root.right = invertTree(temp)
+  return root
 };
