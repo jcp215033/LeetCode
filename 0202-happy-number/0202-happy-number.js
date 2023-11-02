01d2da571e65f9a5572e55f91714a239ce882dac
Julian Castro <jcas2tur@gmail.com>
2023-11-01 22:07:47 -0400

    Time: 59 ms (48.19%), Space: 43 MB (71.25%) - LeetHub

--- 0202-happy-number/0202-happy-number.js
+++ 0202-happy-number/0202-happy-number.js
@@ -5,9 +5,8 @@
 var isHappy = function(n) {
     n = String(n)
     let memo = {}
-    let dp = n => String(n.split('').map(e => Number(e)**2).reduce((a,b) => a + b))
     while(!memo[n]) {
-        memo[n] = dp(n)
+        memo[n] = String(n.split('').map(e => Number(e)**2).reduce((a,b) => a + b))
         n = memo[n]
         if (n === '1') return true
     }
