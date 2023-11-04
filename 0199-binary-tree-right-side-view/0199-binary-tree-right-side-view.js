81184503c2acde848a20d2555a6929519ed6f3fa
Julian Castro <jcas2tur@gmail.com>
2023-11-03 22:03:59 -0400

    Time: 55 ms (54.94%), Space: 43.7 MB (55.41%) - LeetHub

---
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.js
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[]}
+ */
+var rightSideView = function(root) {
+    let res = []
+    if (!root) return res
+    let deque = []
+    deque.push(root)
+    while (deque.length) {
+        let len = deque.length
+        let lvl = []
+        for (let i = 0; i < len; i++) {
+            let curr = deque.shift()
+            if (curr) {
+                lvl.push(curr.val)
+            if (curr.left) deque.push(curr.left)
+            if (curr.right) deque.push(curr.right)
+            }
+        }
+        res.push(lvl)
+    }
+    return res.map(e => e[e.length - 1])
+};
