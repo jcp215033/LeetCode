ffcdf2c5652cb380fa50f4d61e8129522dc9eba8
Julian Castro <jcas2tur@gmail.com>
2023-11-04 08:50:44 -0400

    Time: 62 ms (8.22%), Space: 44.7 MB (5.11%) - LeetHub

--- 1802-number-of-students-unable-to-eat-lunch/1802-number-of-students-unable-to-eat-lunch.js
+++ 1802-number-of-students-unable-to-eat-lunch/1802-number-of-students-unable-to-eat-lunch.js
@@ -3,21 +3,66 @@
  * @param {number[]} sandwiches
  * @return {number}
  */
+
+class Node {
+    constructor(val, next) {
+        this.val = val !== undefined ? val : undefined
+        this.next = next !== undefined ? next : null
+    }
+}
+
+class NewQueue {
+    constructor() {
+        this.head = null
+        this.tail = null
+        this.length = 0;
+    }
+    enqueue(val) {
+        if (!this.head) this.head = this.tail = new Node(val)
+        else {
+            this.tail.next = new Node(val)
+            this.tail = this.tail.next
+        }
+        this.length++
+    }
+
+    dequeue() {
+        if (!this.head) return null
+        const val = this.head.val
+        this.head = this.head.next
+        this.length--
+        if (!this.head) this.tail = null
+        return val
+    }
+
+    peek() {
+        return this.head ? this.head.val : null
+    }
+
+    isEmpty() {
+        return this.length === 0
+    }
+}
+
+
 var countStudents = function(students, sandwiches) {
+    let queue = new NewQueue();
+    let stack = new NewQueue();
+    for (let student of students) queue.enqueue(student);
+    for (let sandwich of sandwiches) stack.enqueue(sandwich);
+
     let count = 0;
-    while (sandwiches.length || students.length) {
-        if (students[0] === sandwiches[0]) {
-            students.shift()
-            sandwiches.shift()
-            count = 0
+    while (!queue.isEmpty() || !stack.isEmpty()) {
+        if (queue.peek() === stack.peek()) {
+            queue.dequeue();
+            stack.dequeue();
+            count = 0;
         } else {
-            students.push(students.shift())
+            queue.enqueue(queue.dequeue())
             count++
         }
-        if (count === students.length) return count
+
+        if (count === queue.length) return count
     }
     return 0
 };
-count = 0
-students = [1,1,1,0,0,1]
-sandwiches = [1,0,0,0,1,1]
