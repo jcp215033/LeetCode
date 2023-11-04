8d9a62558fefccd028cdbf738c1a5af2f73dfb95
Julian Castro <jcas2tur@gmail.com>
2023-11-04 09:04:12 -0400

    Time: 56 ms (25.55%), Space: 41.6 MB (75.36%) - LeetHub

---
+++ 0812-rotate-string/0812-rotate-string.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} s
+ * @param {string} goal
+ * @return {boolean}
+ */
+var rotateString = function(s, goal) {
+    for (let i = 0; i < s.length; i++) {
+        s = s.substring(1) + s[0]
+        if (s === goal) return true
+    }
+    return false
+};
